0875479455895c978669c8bfe0b0ac6dedfea7f6
shashi singh <shashi.bhushan@gmail.com>
2013-08-06 17:33:23 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onemrails::Application.config.secret_token = '08d7f2834fd8ed81b136488b68987cc53563acc44f474d34329efc289ac2a9b8d40313e3710dc85081dfaef590353f12e4bb4a7c3a57a28b7687b3f9965be70e'
